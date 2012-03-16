b88ace1f683924f462c60af81eee1504e6319a51
Steve Masterman <steve@vermonster.com>
2012-03-16 11:05:47 -0400

    add index on key column

--- db/migrate/20120313191745_create_copycat_translations.rb
+++ db/migrate/20120313191745_create_copycat_translations.rb
@@ -1,9 +1,12 @@
 class CreateCopycatTranslations < ActiveRecord::Migration
   def up
     create_table :copycat_translations do |t|    
-      t.string "key"
-      t.text "value"
+      t.string :key
+      t.text :value
       t.timestamps
+    end
+    change_table :copycat_translations do |t|
+      t.index :key
     end
   end
 
