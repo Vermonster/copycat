16d4b9bc0e8b80d0ad9163850c26d5045f3ecc09
Steve Masterman <steve@vermonster.com>
2012-03-20 13:50:56 -0400

    improve detection of missing database/table on Rails bootup/Heroku assets:precompile

--- lib/copycat.rb
+++ lib/copycat.rb
@@ -3,20 +3,14 @@ require "copycat/engine"
 module CopycatImplementation
   # this method overrides part of the i18n gem, lib/i18n/backend/simple.rb
   def lookup(locale, key, scope = [], options = {})
-    begin
-      cct = CopycatTranslation.where(locale: locale, key: key).first
-    rescue ActiveRecord::StatementInvalid
-      raise if CopycatTranslation.table_exists?  
-      #assert Rails is initializing for the purpose of running the copycat_translations migration
-      super 
-    else
-      return cct.value if cct
-      value = super(locale, key, scope, options)
-      if value.is_a?(String) || value.nil?
-        CopycatTranslation.create(locale: locale, key: key, value: value)
-      end
-      value
+    return super unless ActiveRecord::Base.connected?
+    cct = CopycatTranslation.where(locale: locale, key: key).first
+    return cct.value if cct
+    value = super(locale, key, scope, options)
+    if value.is_a?(String) || value.nil?
+      CopycatTranslation.create(locale: locale, key: key, value: value)
     end
+    value
   end
 end
 
