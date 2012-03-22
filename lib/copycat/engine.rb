a1301b56b9d1d79c2ea2575d30932eda592ff589
Andrew Ross <andrew@vermonster.com>
2012-03-22 13:15:02 -0400

    precompile copycat_engine.css when deploying to heroku
    The problem before was that "initialize_on_precompile" was set to false. That variable causes only part of the rails env to be run -- specifically, only the initializers under the group 'assets.' Even though the initializer was getting created, it wasn't getting run because it's group was set to some default value. Adding it to the :all group causes it to always be run, and copycat_engine.css is always precompiled.

--- lib/copycat/engine.rb
+++ lib/copycat/engine.rb
@@ -1,10 +1,11 @@
 module Copycat
   class Engine < ::Rails::Engine
-    # Enabling assets precompiling under rails 3.1
+
     if Rails.version >= '3.1'
-      initializer :assets do |config|
-        Rails.application.config.assets.precompile += %w( copycat_engine.css )
+      initializer :assets, :group => :all do
+        Rails.application.config.assets.precompile += %w(copycat_engine.css)
       end
     end
+
   end
 end
