2acb4c03c1ae12cf2e706c75f21f90a9ab6ba155
Andrew Ross <andrew@vermonster.com>
2012-03-22 10:37:40 -0400

    precompile assets, attempt 1

--- lib/copycat/engine.rb
+++ lib/copycat/engine.rb
@@ -1,4 +1,10 @@
 module Copycat
   class Engine < ::Rails::Engine
+    # Enabling assets precompiling under rails 3.1
+    if Rails.version >= '3.1'
+      initializer :assets do |config|
+        Rails.application.config.assets.precompile += %w( copycat_engine.css )
+      end
+    end
   end
 end
