f68f2f9c69bb4a5b35da86d693e0f67e6206b373
Steve Masterman <steve@vermonster.com>
2012-08-13 16:24:02 -0400

    fix unfortunate bug that PG driver does not like

--- lib/copycat/implementation.rb
+++ lib/copycat/implementation.rb
@@ -3,11 +3,11 @@ module Copycat
     # this method overrides part of the i18n gem, lib/i18n/backend/simple.rb
     def lookup(locale, key, scope = [], options = {})
       return super unless ActiveRecord::Base.connected? && CopycatTranslation.table_exists?
-      cct = CopycatTranslation.where(locale: locale, key: key).first
+      cct = CopycatTranslation.where(locale: locale.to_s, key: key.to_s).first
       return cct.value if cct
       value = super(locale, key, scope, options)
       if value.is_a?(String) || value.nil?
-        CopycatTranslation.create(locale: locale, key: key, value: value)
+        CopycatTranslation.create(locale: locale.to_s, key: key.to_s, value: value)
       end
       value
     end
